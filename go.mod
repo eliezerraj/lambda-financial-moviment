7474454e97131666d27df4db211aa5d686a5ef71
eliezerraj <eliezerraj@gmail.com>
2023-03-22 01:17:32 -0300

    add auth iamrole

--- go.mod
+++ go.mod
@@ -4,11 +4,23 @@ go 1.18
 
 require (
 	github.com/aws/aws-sdk-go v1.44.224
+	github.com/aws/aws-sdk-go-v2 v1.17.7
+	github.com/aws/aws-sdk-go-v2/config v1.18.19
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/rs/zerolog v1.29.0
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.18 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.31 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.32 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.7 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- internal/adapter/restapi/financial_moviment_restapi.go
+++ internal/adapter/restapi/financial_moviment_restapi.go
@@ -1,13 +1,18 @@
 package restapi
 
 import(
-	//"fmt"
+	"fmt"
 	"net/http"
 	"time"
 	"encoding/json"
+	"context"
+	"crypto/sha256"
 
 	"github.com/rs/zerolog/log"
 	"github.com/mitchellh/mapstructure"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 
 	"github.com/lambda-financial-moviment/internal/core/domain"
 	"github.com/lambda-financial-moviment/internal/erro"
@@ -32,7 +37,8 @@ func (r *FinancialMovimentRestApi) GetPersonData(person domain.Person) (*domain.
 	childLogger.Debug().Msg("GetPersonData")
 
 	url := r.Url + r.PersonPath +"/" + person.ID
-	person_interface, err :=makeGet(url, person)
+	//person_interface, err :=makeGet(url, person)
+	person_interface, err :=makeGetAuthIAMRole(url, person)
 	if err != nil {
 		childLogger.Error().Err(err).Msg("error Request")
 		return nil, err
@@ -60,20 +66,92 @@ func makeGet(url string, inter interface{}) (interface{}, error) {
 	}
 
 	req.Header.Add("Content-Type", "application/json;charset=UTF-8");
+	req.Header.Add("x-api-key", "NvbtUByZfK3PO0trigViL2OSQPlx8KTMa8pszPgt");
+
 	resp, err := client.Do(req)
 	if err != nil {
 		childLogger.Error().Err(err).Msg("error Do Request")
 		return false, erro.ErrHTTPRequest
 	}
 
+	childLogger.Debug().Int("StatusCode :", resp.StatusCode).Msg("----")
 	switch (resp.StatusCode) {
 		case 401:
+			return false, erro.ErrHTTPForbiden
+		case 403:
 			return false, erro.ErrHTTPForbiden
 		case 200:
 		case 404:
 			return false, erro.ErrNotFound
 		default:
+			return false, erro.ErrHTTPForbiden
+	}
+
+	result := inter
+	err = json.NewDecoder(resp.Body).Decode(&result)
+    if err != nil {
+		childLogger.Error().Err(err).Msg("error no ErrUnmarshal")
+		return false, erro.ErrUnmarshal
+    }
+
+	return result, nil
+}
+
+func makeGetAuthIAMRole(url string, inter interface{}) (interface{}, error) {
+	childLogger.Debug().Msg("makeGetAuthIAMRole")
+
+	// ---------------------------
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		childLogger.Error().Err(err).Msg("error get Context")
+		return false, erro.ErrHTTPRequest
+	}
+	credentials, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		childLogger.Error().Err(err).Msg("error get Credentials")
+		return false, erro.ErrHTTPRequest
+	}
+	fmt.Println("***** ",credentials," === ",cfg.Region)
+	//--------------------------
+
+	client := &http.Client{Timeout: time.Second * 29}
+	
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		childLogger.Error().Err(err).Msg("error Request")
+		return false, erro.ErrHTTPRequest
+	}
+
+	req.Header.Add("Content-Type", "application/json;charset=UTF-8");
+	req.Header.Add("x-api-key", "NvbtUByZfK3PO0trigViL2OSQPlx8KTMa8pszPgt");
+	//-------------
+	hash := sha256.Sum256([]byte(""))
+	hexHash := fmt.Sprintf("%x", hash)
+
+	signer := v4.NewSigner()
+	err = signer.SignHTTP(context.TODO(), credentials, req, hexHash, "execute-api", cfg.Region, time.Now())
+	if err != nil {
+		childLogger.Error().Err(err).Msg("error signer with credentials")
+		return false, erro.ErrHTTPRequest
+	}
+	//-------------
+	resp, err := client.Do(req)
+	if err != nil {
+		childLogger.Error().Err(err).Msg("error Do Request")
+		return false, erro.ErrHTTPRequest
+	}
+
+	childLogger.Debug().Int("StatusCode :", resp.StatusCode).Msg("----")
+	switch (resp.StatusCode) {
+		case 401:
+			return false, erro.ErrHTTPForbiden
+		case 403:
+			return false, erro.ErrHTTPForbiden
+		case 200:
+		case 404:
 			return false, erro.ErrNotFound
+		default:
+			return false, erro.ErrHTTPForbiden
 	}
 
 	result := inter
